83ff9f71443a366327da62b6b9747be65347a4a1
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-08-01 13:51:21 -0400

    chugging thru review with loops

---
+++ scrapsSevenThirty.js
@@ -0,0 +1,13 @@
+//7-30-2020 MO REVIEW => LOOPIN
+let tenHellos = () => {
+    for(let i = 0; i <= 10; i++) {
+        console.log('Hello.');
+    }
+}
+let squareIt = () => {
+    for(let num = 1; num <= 20; num++) {
+        console.log(`${num}x${num} = ${num * num}`);
+    }
+}
+squareIt();
+
---
+++ scrapsSevenThirtyOne.js
@@ -0,0 +1,2 @@
+// 7-31-2020 LOOPIN AND LOOPS LOOPS LOOPS
+    //for loop => define a variable as the counter for the loop
