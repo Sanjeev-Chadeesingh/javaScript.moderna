c17ec40347182b39d61c67f9bb2bd02557c2f926
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-08-14 10:25:22 -0400

    reviewing functions and going well!

--- scraps.js
+++ scraps.js
@@ -1,8 +1,20 @@
-function outer() {
-    let movie = 'Amadeus';
-    function inner() {
-        console.log(movie.toUpperCase());
-    }
-    inner();
+function picker(arr) {
+    let idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
 }
-outer();
+
+
+
+
+function getCard() {
+    let suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+    let values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    let suit = picker(suits);
+    let value = picker(values);
+    return {
+        value,
+        suit
+    };
+}
+console.log(getCard());
+// getCard();
---
+++ scrapsArrayCBForEach.js
@@ -0,0 +1,2 @@
+//8-14-2020 FOREACH *accepts a cb function|calls function once per each element in the array
+const nums = [20, 21, 22, 23, 24, 25, 26, 27];
