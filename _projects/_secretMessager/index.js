7cc5e157e7b842ce65716259e57eeb398920dd7f
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-08 13:37:15 -0500

    inputs updated in form, added form. btoa function used for encryption

--- _projects/_secretMessager/index.js
+++ _projects/_secretMessager/index.js
@@ -2,6 +2,9 @@ document.querySelector('form').addEventListener('submit', e => {
     //prevents browser from attempting to submit form to backend 
     e.preventDefault()
     //storing input from form on console
-    const input = document.querySelector('input');
-    console.log(input.value);
+    const input = document.querySelector('.message-input');
+    //encrypting the value from the messageInput form 
+    const encrypted = btoa(input.value);
+    //passing newly encrypted message 
+    document.querySelector('.link-input').value = encrypted;
 });
