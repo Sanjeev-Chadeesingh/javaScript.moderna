159a12e40e6b1b71b2373fb2ae79468da2cc919b
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-09 10:49:30 -0500

    window.location with hash, select() method added

--- _projects/_secretMessager/index.js
+++ _projects/_secretMessager/index.js
@@ -4,5 +4,9 @@ document.querySelector('form').addEventListener('submit', e => {
     //select input element, log value
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
-    document.querySelector('#link-input').value = encrypted;
+    //adding the encrypted message to the hash of a stringified window.location
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    //auto selects the link for easier copying
+    linkInput.select();
 })
