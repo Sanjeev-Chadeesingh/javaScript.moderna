8d678844e85474066cab64e4cbd3c9e0ccfc1b10
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-10 12:35:43 -0500

    nls fixed, 2 bad and 1 good solution

--- _projects/_node/_list/index.js
+++ _projects/_node/_list/index.js
@@ -1,20 +1,42 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util');
 
-fs.readdir(process.cwd(), (err, fileNames) => {
+//Method 2
+// const lstat = util.promisify(fs.lstat);
+
+//Method 3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, fileNames) => {
     if(err) {
         console.log(err);
     }
 
-    // BAD CODE HERE
-    for (let fileName of fileNames) {
-        fs.lstat(fileName, (err, stats) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log(fileName, stats.isFile());
-        })
+    const statPromises = fileNames.map((fileName) => {
+        return lstat(fileName);
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+        console.log(fileNames[index], stats.isFile());
     }
-    // BAD CODE COMPLETE
+
 });
+
+
+
+// const lstat = (fileName) => {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(fileName, (err, stats) => {
+//             if (err) {
+//                 reject(err);
+//             }
+
+//             resolve(stats);
+//         })
+//     })
+// };
