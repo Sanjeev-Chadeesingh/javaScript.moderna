749a85a4cd00811e0f03fa94f0b481cedae22fa8
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-10 08:33:54 -0500

    module exports and require cache

--- _projects/_node/index.js
+++ _projects/_node/index.js
@@ -1,6 +1,16 @@
-//this require statement is a relative path to the file needed to run this file
+// //this require statement is a relative path to the file needed to run this file
 // const message = require('./myScript');
 
 // console.log(message);
 
-console.log(require);
+// // console.log(require);
+const counterObject = require('./myScript.js');
+
+console.log(counterObject.getCounter());
+counterObject.incrementCounter();
+console.log(counterObject.getCounter());
+
+const newCounterObject = require('./myScript.js');
+console.log(newCounterObject.getCounter());
+//total output: 0 newLine 1 newLine 1
+    //second require statement is going in order, so one gets logged twice
--- _projects/_node/myScript.js
+++ _projects/_node/myScript.js
@@ -1,4 +1,15 @@
-const message = 'Hiyah There!';
+// const message = 'Hiyah There!';
 
-//this is now available to other files
-module.exports = message;
+// //this is now available to other files
+// module.exports = message;
+
+let counter = 0;
+
+module.exports = {
+    incrementCounter() {
+        counter += 1;
+    },
+    getCounter() {
+        return counter;
+    }
+};
