3d95ec6fcb2d4a6bde5e5783b489d0e3f04fd2e8
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-13 11:37:30 -0500

    watchit further, fs system and other shit added

--- _projects/watchit/index.js
+++ _projects/watchit/index.js
@@ -3,13 +3,29 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const prog = require('caporal');
+const fs = require('fs');
 
-const start = debounce(() => {
-    console.log('Starting user program');
-}, 100);
+prog
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action(async ({ filename }) => {
+        const name = filename || 'index.js';
+        try {
+            await fs.promises.access(name);
+        } catch (err) {
+            throw new Error(`Could not find the file ${name}`);
+        }
+        
+        const start = debounce(() => {
+            console.log('Starting user program');
+        }, 100);
+        
+        chokidar
+            .watch('.')
+            .on('add', start)
+            .on('change', start)
+            .on('unlink', start); 
+    });
 
-chokidar
-    .watch('.')
-    .on('add', start)
-    .on('change', () => console.log('File Changed'))
-    .on('unlink', () => console.log('File Unlinked')); 
+    prog.parse(process.argv);
+
