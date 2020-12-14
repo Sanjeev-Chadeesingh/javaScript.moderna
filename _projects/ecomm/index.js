30c347a0ae14d9fdb981df8ee9ae3c1be7b45fe7
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-14 11:39:31 -0500

    form handling with inputs and post

--- _projects/ecomm/index.js
+++ _projects/ecomm/index.js
@@ -3,9 +3,22 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hi there!');
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>sign up</button>
+            </form>
+        </div>
+    `);
     //request to root (/) will send response of 'hi there!'
 });
+
+app.post('/', (req, res) => {
+    res.send('Account created!');
+})
 
 app.listen(3000, () => {
     console.log('Listening');
