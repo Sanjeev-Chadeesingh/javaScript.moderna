1cedf582cda099d54948c5f674e23dfd62eb7859
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-14 11:18:14 -0500

    express and requests further understood, notes

--- _projects/ecomm/index.js
+++ _projects/ecomm/index.js
@@ -4,8 +4,9 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('hi there!');
-})
+    //request to root (/) will send response of 'hi there!'
+});
 
 app.listen(3000, () => {
     console.log('Listening');
-})
+});
