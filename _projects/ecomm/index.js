0cbcfcfa0cdf5458cf87bf692a717d7cb04a2a59
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-14 12:23:14 -0500

    handmade middleware helper function

--- _projects/ecomm/index.js
+++ _projects/ecomm/index.js
@@ -16,18 +16,31 @@ app.get('/', (req, res) => {
     //request to root (/) will send response of 'hi there!'
 });
 
-app.post('/', (req, res) => {
-    req.on('data', (data) => {
-        const parsed = data.toString('utf8').split('&');
-        const formData = {};
-        for (let pair of parsed) {
-            const [key, value] = pair.split('=');
-            formData[key] = value;
-        }
-        console.log(formData);
-    });
+//middleware function, always 3 args (req, res, next)
+    //next is a callback given to us from express framework
+const bodyParser = (req, res, next) => {
+    if (req.method === 'POST') {
+        req.on('data', (data) => {
+            const parsed = data.toString('utf8').split('&');
+            const formData = {};
+            for (let pair of parsed) {
+                const [key, value] = pair.split('=');
+                formData[key] = value;
+            }
+            req.body = formData;
+            //letting express know we are done
+            next();
+        });
+    } else {
+        //this is signal we are done with processing
+        next();
+    }
+}
+
+app.post('/', bodyParser, (req, res) => {
+    console.log(req.body);
     res.send('Account created!');
-})
+});
 
 app.listen(3000, () => {
     console.log('Listening');
