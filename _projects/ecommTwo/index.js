e04afc57c614e758eab6f64c4904ce4be2a2f54e
Sanjeev Chadeesingh <sanjeev.chadeesingh@gmail.com>
2020-12-16 11:08:23 -0500

    handMade middleware for req.data

--- _projects/ecommTwo/index.js
+++ _projects/ecommTwo/index.js
@@ -29,7 +29,14 @@ app.post('/', (req, res) => {
             //the data log is the req body in byte form
     req.on('data', (data) => {
             //now logging data and changing it into a string!
-        console.log(data.toString('utf8'));
+        // console.log(data.toString('utf8'));
+        const parsed = data.toString('utf8').split('&');
+        const formData = {};
+        for (let pair of parsed) {
+            const [key, value] = pair.split('=');
+            formData[key] = value;
+        }
+        console.log(formData);
     })
     res.send('Account Created!');
 });
